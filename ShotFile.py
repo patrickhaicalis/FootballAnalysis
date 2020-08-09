570fdcc3f91a276a0dc96bd59805ad05bcac7edf
patrickhaicalis <54026477+patrickhaicalis@users.noreply.github.com>
2020-08-09 16:10:21 +0100

    Update ShotFile.py

--- ShotFile.py
+++ ShotFile.py
@@ -2,12 +2,14 @@ from Test import understat_json
 from PulisicShots import ShotResult
 from TypeOfShot import TypeOfShot
 from Heatmap import Heatmapshots
+from HeatmapGoal import Heatmapgoals
 
-name = 'James'
-id = 8067
+name = 'Salah'
+id = 1250
 
 understat_json(name, id)
 ShotResult(name)
 TypeOfShot(name)
 Heatmapshots(name)
+Heatmapgoals(name)
 print('All done!')
