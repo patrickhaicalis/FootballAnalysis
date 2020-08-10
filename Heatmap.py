511268364a1fd4e493316c8dece7166dac90df9e
patrickhaicalis <54026477+patrickhaicalis@users.noreply.github.com>
2020-08-10 17:40:10 +0100

    Update Heatmap.py

--- Heatmap.py
+++ Heatmap.py
@@ -11,7 +11,7 @@ import numpy as np
 
 def Heatmapshots(name):
 
-    im = Image.open(f'Images/{name}Shots.png')
+    im = Image.open(f'Images/Shots/{name}Shots.png')
 
     with open(f'jsonfiles/{name}.json') as data_file:
         data = json.load(data_file)
@@ -19,11 +19,11 @@ def Heatmapshots(name):
     df = pd.read_json(data)
 
     f, ax = CreatePitch()
-    cmap = LinearSegmentedColormap.from_list("", ["blue","green","yellow","red"])
+    cmap = LinearSegmentedColormap.from_list("", ['black', "blue","green","yellow","red"])
     sns.kdeplot(1-df.Y, df.X, cmap=cmap, n_levels=50, shade=True, shade_lowest=False)
-    plt.savefig(f'Images/{name}ShotHeatmap.png')
+    plt.savefig(f'Images/ShotHeatmap/{name}ShotHeatmap.png')
 
-    m =  cv2.imread(f"Images/{name}ShotHeatmap.png")
+    m =  plt.imread(f"Images/ShotHeatmap/{name}ShotHeatmap.png")
 
     h,w,bpp = np.shape(m)
 
@@ -39,5 +39,6 @@ def Heatmapshots(name):
                 m[py][px][1] = 0
                 m[py][px][2] = 0
 
-    cv2.imshow('matrix', m)
-    cv2.imwrite(f'Images/{name}ShotHeatmap.png',m)
+    plt.title(f'{name} Shot Heatmap 2019/20 Season')
+    plt.savefig(f'Images/ShotHeatmap/{name}ShotHeatmapTEST.png')
+    plt.show()
