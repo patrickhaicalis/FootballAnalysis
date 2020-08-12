90c4ba27995781937ae855adb6ad3b8e7789985c
patrickhaicalis <54026477+patrickhaicalis@users.noreply.github.com>
2020-08-12 16:38:08 +0100

    Update ShotFile.py

--- ShotFile.py
+++ ShotFile.py
@@ -3,6 +3,7 @@ from PulisicShots import ShotResult
 from TypeOfShot import TypeOfShot
 from Heatmap import Heatmapshots
 from HeatmapGoal import Heatmapgoals
+from Combined import CombinedReport
 
 name = 'Salah'
 id = 1250
@@ -12,4 +13,6 @@ ShotResult(name)
 TypeOfShot(name)
 Heatmapshots(name)
 Heatmapgoals(name)
+CombinedReport(name)
+
 print('All done!')
